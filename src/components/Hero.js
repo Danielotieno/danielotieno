ce96083ed723e4fd6a14f7f288da67b37633676a
Daniel Otieno <danielotieno.ke@gmail.com>
2020-06-09 15:05:38 +0300

    chore:creates hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,47 @@
-import React from "react"
-import Image from "gatsby-image"
-import { Link } from "gatsby"
-import { graphql, useStaticQuery } from "gatsby"
-import SocialLinks from "../constants/socialLinks"
-// ...GatsbyImageSharpFluid
+import React from 'react';
+import Image from 'gatsby-image';
+import { Link } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
+import SocialLinks from '../constants/socialLinks';
+
+const profileImageQuery = graphql`
+  {
+    file(relativePath: { eq: "dp.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
 
 const Hero = () => {
-  return <h2>hero component</h2>
-}
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(profileImageQuery);
+  return (
+    <header className='hero'>
+      <div className='section-center hero-center'>
+        <article className='hero-info'>
+          <div className='span-heading'>
+            <span>Hello I'm</span>
+          </div>
+          <div>
+            <h1>Daniel Otieno</h1>
+            <h4>🔥 Software Developer</h4>
+            <Link to='/contact' className='btn'>
+              Cantact Me
+            </Link>
+            <SocialLinks />
+          </div>
+        </article>
+        <Image fluid={fluid} className='hero-img' />
+      </div>
+    </header>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -1,51 +1,45 @@
-import React from "react"
+import React from 'react';
 import {
-  FaFacebookSquare,
   FaLinkedin,
-  FaDribbbleSquare,
-  FaBehanceSquare,
+  FaGithubSquare,
+  FaStackOverflow,
   FaTwitterSquare,
-} from "react-icons/fa"
+} from 'react-icons/fa';
 
 const data = [
   {
     id: 1,
-    icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: "https://www.twitter.com",
+    icon: <FaGithubSquare className='social-icon'></FaGithubSquare>,
+    url: 'https://github.com/danielotieno',
   },
   {
     id: 2,
-    icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: "https://www.twitter.com",
+    icon: <FaStackOverflow className='social-icon'></FaStackOverflow>,
+    url: 'https://stackoverflow.com/users/9635966/daniel-otieno',
   },
   {
     id: 3,
-    icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
-    url: "https://www.twitter.com",
+    icon: <FaLinkedin className='social-icon'></FaLinkedin>,
+    url: 'https://www.linkedin.com/in/danielotieno/',
   },
   {
     id: 4,
-    icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
-    url: "https://www.twitter.com",
+    icon: <FaTwitterSquare className='social-icon'></FaTwitterSquare>,
+    url: 'https://twitter.com/danielotienoke',
   },
-  {
-    id: 5,
-    icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
-    url: "https://www.twitter.com",
-  },
-]
-const links = data.map(link => {
+];
+const links = data.map((link) => {
   return (
     <li key={link.id}>
-      <a href={link.url} className="social-link">
+      <a href={link.url} className='social-link'>
         {link.icon}
       </a>
     </li>
-  )
-})
+  );
+});
 
 export default ({ styleClass }) => {
   return (
-    <ul className={`social-links ${styleClass ? styleClass : ""}`}>{links}</ul>
-  )
-}
+    <ul className={`social-links ${styleClass ? styleClass : ''}`}>{links}</ul>
+  );
+};
