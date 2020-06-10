0ad588e0d7ff12524ccff1a0867f2730ef11a479
Daniel Otieno <danielotieno.ke@gmail.com>
2020-06-10 14:36:22 +0300

    chore:update service section

--- src/constants/services.js
+++ src/constants/services.js
@@ -4,19 +4,16 @@ export default [
   {
     id: 1,
     icon: <FaCode className='service-icon' />,
-    title: 'Software development',
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    title: 'Web development',
   },
   {
     id: 2,
     icon: <FaChartLine className='service-icon' />,
     title: 'Data Science',
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
   },
   {
     id: 3,
     icon: <FaPeopleCarry className='service-icon' />,
     title: 'Community Work',
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
   },
 ];
