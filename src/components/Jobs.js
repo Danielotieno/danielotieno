73a48f6ff88bc7546a40340bfc69aa7c55ac7421
Daniel Otieno <danielotieno.ke@gmail.com>
2020-06-12 23:51:47 +0300

    chore:display job data

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -8,6 +8,7 @@ const query = graphql`
   {
     allStrapiJobs(sort: { fields: strapiId, order: DESC }) {
       nodes {
+        strapiId
         company
         date
         position
@@ -22,7 +23,47 @@ const query = graphql`
 `;
 
 const Jobs = () => {
-  return <h2>jobs component</h2>;
+  const data = useStaticQuery(query);
+  const {
+    allStrapiJobs: { nodes: jobs },
+  } = data;
+  const [value, setValue] = React.useState(0);
+  const { company, date, position, desc } = jobs[value];
+  return (
+    <section className='section jobs'>
+      <Title title='experience' />
+      <div className='jobs-center'>
+        <div className='btn-container'>
+          {jobs.map((job, index) => {
+            return (
+              <button
+                key={job.strapiId}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index == value && 'active-btn'}`}>
+                {job.company}
+              </button>
+            );
+          })}
+        </div>
+        <article className='job-info'>
+          <h3>{position}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{date}</p>
+          {desc.map((item) => {
+            return (
+              <div key={item.id} className='job-desc'>
+                <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight>
+                <p>{item.name}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+      <Link to='/about' className='btn center btn'>
+        more info
+      </Link>
+    </section>
+  );
 };
 
 export default Jobs;
