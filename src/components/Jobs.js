3c77161d29c67b6475f4920bf719a7de41f4a433
Daniel Otieno <danielotieno.ke@gmail.com>
2020-06-12 23:19:56 +0300

    chore:setup query to fetch experience data

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,11 +1,28 @@
-import React from "react"
-import Title from "./Title"
-import { FaAngleDoubleRight } from "react-icons/fa"
-import { graphql, useStaticQuery } from "gatsby"
-import { Link } from "gatsby"
+import React from 'react';
+import Title from './Title';
+import { FaAngleDoubleRight } from 'react-icons/fa';
+import { graphql, useStaticQuery } from 'gatsby';
+import { Link } from 'gatsby';
+
+const query = graphql`
+  {
+    allStrapiJobs(sort: { fields: strapiId, order: DESC }) {
+      nodes {
+        company
+        date
+        position
+        id
+        desc {
+          id
+          name
+        }
+      }
+    }
+  }
+`;
 
 const Jobs = () => {
-  return <h2>jobs component</h2>
-}
+  return <h2>jobs component</h2>;
+};
 
-export default Jobs
+export default Jobs;
