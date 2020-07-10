4359447a44c78458a5180b62c7fe861b54a189be
danielotieno <danielotieno.ke@gmail.com>
2020-07-10 18:30:54 +0300

    creates footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,15 @@
-import React from "react"
-import SocialLinks from "../constants/socialLinks"
+import React from 'react';
+import SocialLinks from '../constants/socialLinks';
 const Footer = () => {
-  return <h2>footer component</h2>
-}
+  return (
+    <footer className='footer'>
+      <div>
+        <SocialLinks styleClass='footer-links' />
+        <h4>copyright&copy;{new Date().getFullYear()}</h4>
+        <span className='daniel'>Daniel Otieno &#10084;&#65039;</span>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
